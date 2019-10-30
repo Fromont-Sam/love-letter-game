10aaad4292c10673253fac694653a13a3eaf61e4
skydex <fromont.sam@gmail.com>
2019-10-30 23:58:11 +0100

    modyfing metada

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -19,6 +19,7 @@ export default class Routes {
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <meta name="google-site-verification" content="GwaUTLjbdJRL46OBU2iqZtHwJ_bfFwSvD3ka0vcCZ-M" />
           <title>Phaser 3: Multiplayer Example</title>
         </head>
         <body>
